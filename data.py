57771213394eedf036c258149530cf860ecbc135
kthfan <58523053+kthfan@users.noreply.github.com>
2023-10-04 00:01:25 +0800

    Update data.py, update LabelmeDataset, add PaletteSegmentationDataset

--- data.py
+++ data.py
@@ -59,10 +59,11 @@ class LabelmeDataset(torchvision.datasets.VisionDataset):
             json_roots: str or list of str. The directory of json files.
             image_roots: str or list of str. The directory of images, each filename in image_roots corresponds to the filename in json_roots.
             class_names: str or list of str. The class names will be included, otherwise will be excluded.
+            onehot: If True, the returned label will be one hot encoded.
             transform: Transformation on the images.
             target_transform: Transformation on the masks
     '''
-    def __init__(self, json_roots, image_roots, class_names, transform=None, target_transform=None):
+    def __init__(self, json_roots, image_roots, class_names, onehot=False, transform=None, target_transform=None):
         if isinstance(json_roots, str):
             json_roots = [json_roots]
         if isinstance(image_roots, str):
@@ -71,6 +72,7 @@ class LabelmeDataset(torchvision.datasets.VisionDataset):
         self.json_roots = json_roots
         self.image_roots = image_roots
         self.class_names = np.asarray(class_names)
+        self.onehot = onehot
 
         self.samples = [os.listdir(path) for path in self.json_roots]
         self.samples = [[os.path.join(root, path) for path in paths] for root, paths in
@@ -86,7 +88,9 @@ class LabelmeDataset(torchvision.datasets.VisionDataset):
         with open(path, "r", encoding="utf-8") as f:  # load json
             config = json.load(f)
         masks = LabelmeDataset.extract_labelme_onehot(config, self.class_names)
-        masks = np.argmax(np.pad(masks, [[1, 0], [0, 0], [0, 0]]), axis=0)
+        masks = np.concatenate([~masks.any(axis=0, keepdims=True), masks], axis=0)
+        if not self.onehot:
+            masks = np.argmax(masks, axis=0)
         masks = torch.from_numpy(masks)
 
         img_path = os.path.join(self.image_roots[group_index], config['imagePath'])
@@ -117,7 +121,6 @@ class LabelmeDataset(torchvision.datasets.VisionDataset):
                 pts = pts.reshape((-1, 1, 2))
                 masks[i] = cv2.fillPoly(masks[i], [pts], color=1)
         return masks
-
 
 class BinarySegmentationDataset(torchvision.datasets.VisionDataset):
     '''' Load images and masks from directories.
@@ -154,6 +157,85 @@ class BinarySegmentationDataset(torchvision.datasets.VisionDataset):
         with open(mask_path, "rb") as file:
             mask = Image.open(file)
             mask = mask.convert("L")
+
+        if self.transform is not None:
+            img = self.transform(img)
+        if self.target_transform is not None:
+            mask = self.target_transform(mask)
+
+        return img, mask
+
+    def __len__(self):
+        return len(self.imgs)
+
+class PaletteSegmentationDataset(torchvision.datasets.VisionDataset):
+    '''' Load images and masks from directories,the format of masks is saved in palette (hint: PIL.Image.putpalette).
+        Arguments:
+            image_roots: str or list of str. The directory of images.
+            mask_roots: str or list of str. The directory of masks.
+            image_to_mask: A function transform image file name to mask file name.
+            mask_to_image: A function transform mask file name to image file name.
+            transform: Transformation on the images.
+            target_transform: Transformation on the masks
+    '''
+    def __init__(self, image_roots, mask_roots,
+                 image_to_mask=None, mask_to_image=None,
+                 transform=None, target_transform=None):
+        if isinstance(image_roots, str):
+            image_roots = [image_roots]
+        if isinstance(mask_roots, str):
+            mask_roots = [mask_roots]
+        if image_to_mask is None and mask_to_image is None:
+            image_to_mask = lambda x: x
+        super().__init__(root=image_roots[0], transform=transform, target_transform=target_transform)
+        self.image_roots = image_roots
+        self.mask_roots = mask_roots
+
+        if image_to_mask is not None:
+            filenames = [os.listdir(path) for path in self.image_roots]
+            mask_to_image = lambda x: x
+        else:
+            filenames = [os.listdir(path) for path in self.mask_roots]
+            image_to_mask = lambda x: x
+
+        self.image_paths = [os.path.join(root, mask_to_image(path))
+                            for root, paths in zip(self.image_roots, filenames)
+                            for path in paths]
+        self.mask_paths = [os.path.join(root, image_to_mask(path))
+                           for root, paths in zip(self.mask_roots, filenames)
+                           for path in paths]
+        self.samples = list(zip(self.image_paths, self.mask_paths))
+        self.imgs = self.samples
+
+    @staticmethod
+    def convert_color_to_label_id(src_path, dst_path, colors):
+        os.makedirs(dst_path, exist_ok=True)
+        fns = os.listdir(src_path)
+        colors = np.asarray(colors, dtype=np.uint8).reshape((-1, 3))
+        for fn in tqdm(fns):
+            path = os.path.join(src_path, fn)
+            with open(path, "rb") as file:
+                mask = Image.open(file)
+                mask = mask.convert('RGB')
+            mask = np.array(mask)
+            new_mask = np.zeros(mask.shape[:2], dtype=np.int8)
+            for i, c in enumerate(np.split(colors, colors.shape[0], axis=0)):
+                new_mask[np.all(mask == c[0], axis=-1)] = i
+
+            mask = Image.fromarray(new_mask, mode='P')
+            mask.putpalette(colors)
+
+            mask.save(os.path.join(dst_path, fn))
+
+    def __getitem__(self, index):
+        img_path, mask_path = self.imgs[index]
+
+        with open(img_path, "rb") as file:
+            img = Image.open(file)
+            img = img.convert("RGB")
+        with open(mask_path, "rb") as file:
+            mask = Image.open(file)
+            mask = mask.convert("P")
 
         if self.transform is not None:
             img = self.transform(img)
